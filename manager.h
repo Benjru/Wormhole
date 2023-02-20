60f2f23444f9d554142823004ee64ce2b8e34359
Benjamin Hallihan <ben.hallihan1@gmail.com>
2023-02-20 19:19:05 -0400

    began window tracking implementation

---
+++ manager.h
@@ -0,0 +1,28 @@
+#ifndef MANAGER_H
+#define MANAGER_H
+
+#include <xcb/xcb.h>
+
+extern int BAR_SIZE;
+extern int BORDER_WIDTH;
+extern uint32_t background_color;
+
+/* Struct used for tracking window properties */
+typedef struct 
+{	
+	xcb_window_t parent;
+	xcb_window_t child;
+	int geometry[5];
+} wormhole_window_t;
+
+/* Struct for tracking information about all windows */
+typedef struct // TODO: implement relavent functionality
+{
+	wormhole_window_t *windows;
+	wormhole_window_t focused;
+	int num_windows;
+} manager_data_t;
+
+void wormhole_create_window();
+
+#endif
